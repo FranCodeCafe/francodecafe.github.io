c8980d4cb72bd3852480811e45a68a385ba08735
FranCodeCafe <franciscasalinascastillo@gmail.com>
2020-03-05 12:24:28 -0300

    beta-version v2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ class Navbar extends React.Component{
         return (
             <nav className="navbar is-white" role="navigation" aria-label="main navigation">
                 <div className="navbar-brand">
-                    <a className="navbar-item" href="/"><img src={pink_cup} alt="FranCodeCafe_Logo"/><h1 className="logo-text">FranCodeCafé</h1></a>
+                    <a className="navbar-item" href="/"><img src={pink_cup} alt="FranCodeCafe_Logo"/><h1 className="logo-text">FranCodeCafé</h1><span className="beta">Beta Version</span></a>
 
                     <div className="navbar-burger burger" data-target="Options">
                         <span></span>
