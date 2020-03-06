435811fa31f9199478c194749cbaf3263c7180b9
FranCodeCafe <franciscasalinascastillo@gmail.com>
2020-03-06 10:51:51 -0300

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bulma/css/bulma.css';
-import 'bulma-switch/dist/css/bulma-switch.min.css';
-import './global.css';
-import App from './components/App';
-
-const container = document.getElementById('app');
-ReactDOM.render(<App />, container);
